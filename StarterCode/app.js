875cce54c22ac7fabc9a72fc3402b63beeb5829f
Eddie <eddie.xu.nyc@gmail.com>
2020-12-05 13:50:00 -0500

    updated JS

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -1,56 +1,24 @@
-function buildMetadata(sample) {
+var svgWidth = 960;
+var svgHeight = 500;
 
-    // @TODO: Complete the following function that builds the metadata panel
-  
-    // Use `d3.json` to fetch the metadata for a sample
-      // Use d3 to select the panel with id of `#sample-metadata`
-  
-      // Use `.html("") to clear any existing metadata
-  
-      // Use `Object.entries` to add each key and value pair to the panel
-      // Hint: Inside the loop, you will need to use d3 to append new
-      // tags for each key-value in the metadata.
-  
-      // BONUS: Build the Gauge Chart
-      // buildGauge(data.WFREQ);
-  }
-  
-  function buildCharts(sample) {
-  
-    // @TODO: Use `d3.json` to fetch the sample data for the plots
-  
-      // @TODO: Build a Bubble Chart using the sample data
-  
-      // @TODO: Build a Pie Chart
-      // HINT: You will need to use slice() to grab the top 10 sample_values,
-      // otu_ids, and labels (10 each).
-  }
-  
-  function init() {
-    // Grab a reference to the dropdown select element
-    var selector = d3.select("#selDataset");
-  
-    // Use the list of sample names to populate the select options
-    d3.json("/names").then((sampleNames) => {
-      sampleNames.forEach((sample) => {
-        selector
-          .append("option")
-          .text(sample)
-          .property("value", sample);
-      });
-  
-      // Use the first sample from the list to build the initial plots
-      const firstSample = sampleNames[0];
-      buildCharts(firstSample);
-      buildMetadata(firstSample);
-    });
-  }
-  
-  function optionChanged(newSample) {
-    // Fetch new data each time a new sample is selected
-    buildCharts(newSample);
-    buildMetadata(newSample);
-  }
-  
-  // Initialize the dashboard
-  init();
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+  .select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
